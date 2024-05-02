e5ff68d05b1ac7a077b57ab67c7a4fe7a3b12c9b
Sidó Norbert <sido.norbert93@gmail.com>
2024-05-02 16:09:23 +0200

    Create from

--- src/app/components/CRUD/robot-add/robot-add.component.ts
+++ src/app/components/CRUD/robot-add/robot-add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-robot-add',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './robot-add.component.css'
 })
 export class RobotAddComponent {
+  newRobot: FormGroup;
+  types = [
+    { value: '1', label: 'brawler' },
+    { value: '2', label: 'rouge' },
+    { value: '3', label: 'assault' },
+  ];
 
+  constructor() {
+    this.newRobot = new FormGroup({
+      robotName: new FormControl('', [Validators.required]),
+      robotType: new FormControl(null, [Validators.required]),
+      robotStr: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(100)])
+    });
+  }
+
+  get robotName() { return this.newRobot.get('robotName') };
+  get robotType() { return this.newRobot.get('robotType') };
+  get robotStr() { return this.newRobot.get('robotStr') };
+
+  submit() { }
 }
