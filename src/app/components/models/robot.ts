de533b96bd76bbd77721ac1e48f90029fc14b7be
Sidó Norbert <sido.norbert93@gmail.com>
2024-05-06 13:35:19 +0200

    modification strength type

--- src/app/components/models/robot.ts
+++ src/app/components/models/robot.ts
@@ -2,9 +2,9 @@ export class Robot {
     id: number;
     name: string;
     type: string;
-    strength: string;
+    strength: number;
 
-    constructor(id: number, name: string, type: string, strength: string) {
+    constructor(id: number, name: string, type: string, strength: number) {
         this.id = id;
         this.name = name;
         this.type = type;
